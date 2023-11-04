0afe493f12558e7dae3724216855f8464944b428
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2023-11-04 16:49:45 +0530

    Added Text-2-png

---
+++ Text-to-Handwriting(Not Working)/index.py
@@ -0,0 +1,10 @@
+import pywhatkit as pw
+
+
+
+txt = """
+PyWhatKit is a Python library with various helpful features. It's easy-to-use and does not require you to do any additional setup. Currently, it is one of the most popular library for WhatsApp and YouTube automation. New updates are released frequently with new features and bug fixes.
+"""
+    
+
+pw.text_to_handwriting(txt)
